221e87def46e39196c5d06e5ac5d1ae46b7adbe3
jmcaok <jmchasson@gmail.com>
2024-09-08 12:58:23 +0200

    added a function for add, (+)

---
+++ Calculator/calculator.py
@@ -0,0 +1,38 @@
+import time
+
+
+def main():
+    # take input from user
+    dda = input('please enter an operation. (add, sub, mult, div, per, exp, exit):\t')
+    num1 = float(input('please enter a number:\t'))
+    num2 = float(input('please enter another number:\t'))
+    # pass input to mode
+    print("The answer is:")
+    print(mode(dda, num1, num2))
+    
+    return 0
+
+def mode(function, x, y): 
+    if function == 'add':
+        return add(x, y)
+   # add code for testing what function should be called
+    return #answer
+
+def add(x, y):
+    # add functionality for adding
+    return x+y
+
+def  subtract(x, y):
+    # add code here
+    return #answer
+
+def multiply(x, y):
+    # add code here
+    return #answer
+
+def divide(x, y):
+    # add code here
+    return #answer
+    
+if __name__ == "__main__":
+    main()

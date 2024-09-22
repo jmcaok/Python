05164170f830d1b427a32c1c303add0e8bd6e7c8
jmcaok <jmchasson@gmail.com>
2024-09-22 13:40:33 +0200

    homework for 29 september 2024

---
+++ Vowel counter/vowel.py
@@ -0,0 +1,19 @@
+def main():
+    # accept a word as input
+    # call the count() function on the word
+    # print the number of vowels
+    return 0
+
+def count(word):
+    counter = 0
+    # count number of vowels in word here
+    # you can use a for loop or a while loop
+    # if you use a for loop, you can say: for letter in word:
+    # this will loop through the word tracking each letter until the end of the word
+    # you can then compare each letter to a list of vowels using the in operator 
+    # if you use a while loop, you will have to use a counter which you increase after every iteration until the end of the word
+    # you can get the length of the word using len(word)
+    return #return the number of vowels in the word
+
+if __name__ == "__main__":
+    main()

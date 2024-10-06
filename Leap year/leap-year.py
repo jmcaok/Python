3c445145e8a6c75718eed62367b1b12876ee2eca
aioniaeftychia <72868391+aioniaeftychia@users.noreply.github.com>
2024-10-06 13:32:21 +0200

    Implemented template code

---
+++ Leap year/leap-year.py
@@ -0,0 +1,10 @@
+def main():
+    return 0
+
+def isLeapYear(year):
+    # return True if year is a leap year
+    # return False if year is not a leap year
+    return 
+
+if "__name__" == "__main__":
+    main()
